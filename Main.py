973c71e8ccca9d6eeba6106a163edcaa1e147f1c
Radicrow <igoracatauassu03@gmail.com>
2024-09-17 21:35:37 -0300

    DND to IMDB_MOVIES

--- Main.py
+++ Main.py
@@ -1,4 +1,18 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 import pandas as pd
+import plotly.express as px
 
-monster_df = pd.read_csv('dnd_monsters.csv')
+
+df = pd.read_csv('imdb_movies.csv')
+
+#Fazendo um explode para separar os gêneros
+df['genre'] = df['genre'].str.split(',')
+df = df.explode('genre')
+df['genre'] = df['genre'].str.strip()
+print(df[df['genre']== 'Animation'])
+
+#tratamento de valores nulos
+df.fillna('Unknown', inplace=True)
 
