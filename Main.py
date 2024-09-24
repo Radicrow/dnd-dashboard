ad89a9724f60eabca3e4d9affc2b1b8f5e1d4ced
Enzo Kikuchi <127358329+Jkenji@users.noreply.github.com>
2024-09-24 19:17:32 -0300

    criando aplicacao web

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 import pandas as pd
-#import plotly.express as px
+import plotly.express as px
 import streamlit as st
 
+st.set_page_config(layout="wide")
 
 df = pd.read_csv('imdb_movies.csv')
 
@@ -15,11 +16,11 @@ df = pd.read_csv('imdb_movies.csv')
 #tratamento de valores nulos
 df.fillna('Unknown', inplace=True)
 
-print(df.duplicated().sum())
+#print(df.duplicated().sum())
 
 df = df.rename(columns={'names': 'name', 'budget_x': 'budget_mil', 'revenue': 'revenue_mil', 'date_x': 'date', 'country': 'release_country'})
 
-print(list(df.columns))
+#print(list(df.columns))
 
 df['income_mil'] = (df['revenue_mil'] - df['budget_mil'])/10**6
 
@@ -45,4 +46,24 @@ df = df[df.revenue_mil != 0]
 df = df[df.budget_mil != 0]
 print(df.describe())
 
-print(df.head())
+df['income_tier'] = df['income_mil'].apply(lambda x: 
+                                                "Financial Loss" if x < 0 \
+                                             else "Low" if 0 <= x <= 10 \
+                                             else "Mid" if 10 < x <= 100 \
+                                             else "High" if 100 < x <= 250 \
+                                             else "Very High")
+
+
+p_genre = st.sidebar.selectbox("Genre", df["primary_genre"].unique())
+df_filtered = df[df["primary_genre"] == p_genre]
+
+df_filtered
+col1, col2 = st.columns(2)
+
+fig = px.pie(df_filtered, names ='income_tier', title='Distribution of Income Tiers', color_discrete_sequence=px.colors.sequential.RdBu)
+col1.plotly_chart(fig)
+
+df['income_mil'].median()
+fig = px.bar(df_filtered, x ='', title='Distribution of Income Tiers', color_discrete_sequence=px.colors.sequential.RdBu)
+col1.plotly_chart(fig)
+
